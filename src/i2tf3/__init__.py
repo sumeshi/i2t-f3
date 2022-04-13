74c0dd2d8d3f3d02d57dd728e1417f90aad94c34
sumeshi <sum3sh1@protonmail.com>
2022-04-14 02:54:04 +0900

    feat: add cli

---
+++ src/i2tf3/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from i2tf3.presentation.cli import CliPresenter
+
+
+def run():
+  CliPresenter().run()
+
+
+if __name__ == '__main__':
+  run()
---
+++ src/i2tf3/presentation/cli.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+
+class CliPresenter(object):
+  def __init__(self):
+    pass
+  
+  def run(self):
+    print('i2tf3')
